a18dec995cc4327203844ebefae8d67a547da105
botcodernumber1 <khairulislam7879@gmail.com>
2024-09-18 16:06:45 +0600

    Second Comment

---
+++ Econtact.cs
@@ -0,0 +1,15 @@
+namespace E_Contact
+{
+    public partial class Econtact : Form
+    {
+        public Econtact()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- obj/Debug/net8.0-windows/E-Contact.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/E-Contact.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Contact")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97898020c5280789d41d24a3a60f9b752a9e5e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Contact")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Contact")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
